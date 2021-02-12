72519a3eef37f10d21d7f86f8b93ef88fa972b4d
Amirthaa181 <samirtha79@gmail.com>
2021-02-12 13:27:42 +0530

    As per asked..

---
+++ src/org/data/Employee.java
@@ -0,0 +1,12 @@
+package org.data;
+
+public class Employee {
+
+	public void empId() {
+		System.out.println("Emp Id : 123");
+	}
+	
+	public void empName() {
+		System.out.println("Emp Name : Anu");
+	}
+}
